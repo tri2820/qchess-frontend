c8a3d9c27880b4742b754dcb232d1906a4276b65
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2025-01-13 19:10:19 +0100

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import katex from "katex";
-import "katex/dist/katex.min.css";
 import { createEffect, For, Show } from "solid-js";
 import GameEndedBanner from "~/components/GameEndedBanner";
 import PieceImg from "~/components/PieceImg";
